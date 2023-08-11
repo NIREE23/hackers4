7cecb5fae9869a3537b140a89665f5b4bd7c95bd
pr-th-m <pratham.dv@codecraft.co.in>
2023-08-11 15:01:21 +0530

    feat: added the appropriate function

--- string.js
+++ string.js
@@ -1,1 +1,20 @@
 //Author - Pratham
+const stringFreq=(str, letter)=>
+{
+    let count=0;
+    for(let i=0;i<str.length;i++)
+    {
+        if(str[i]==letter)
+        {
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        console.log("Letter not found!!!");
+    }
+    else
+    {
+        console.log(`${letter} has be found ${count} times`);
+    }
+}
