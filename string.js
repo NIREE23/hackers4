2854c20ef7f9c8a7469dc389dcc43ffb66a9af7e
pr-th-m <pratham.dv@codecraft.co.in>
2023-08-11 15:02:45 +0530

    chore: added a jsDocumentation

--- string.js
+++ string.js
@@ -1,4 +1,10 @@
 //Author - Pratham
+/**
+ * This function takes a string and a letter as a parameter
+ * This function does'nt return anything, instead it prints the frequency of that letter in the passed string
+ * @param {string} str 
+ * @param {character} letter 
+ */
 const stringFreq=(str, letter)=>
 {
     let count=0;
