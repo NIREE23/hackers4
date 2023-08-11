7aa6f58162d723018f566e548d4c4adabc7f5416
pr-th-m <pratham.dv@codecraft.co.in>
2023-08-11 15:11:37 +0530

    chore: added a jsDocumentation

--- hackerproject.js
+++ hackerproject.js
@@ -6,3 +6,22 @@
  * @param {string} str 
  * @param {character} letter 
  */
+const stringFreq=(str, letter)=>
+{
+    let count=0;
+    for(let i=0;i<str.length;i++)
+    {
+        if(str[i]==letter)
+        {
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        console.log("Letter not found!!!");
+    }
+    else
+    {
+        console.log(`${letter} has be found ${count} times`);
+    }
+}
