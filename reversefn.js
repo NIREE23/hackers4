f4d9b0e43dcf83fdd181f2725ae652af5f254427
Nireeksha <nireeksha.rao@codecraft.co.in>
2023-08-11 15:06:51 +0530

    jdocs:Added the documentation

--- reversefn.js
+++ reversefn.js
@@ -1,4 +1,9 @@
-
+/**
+ * This function iterstes through the input string in reverse order and constructs a reversed string by appending each character
+ * 
+ * @param {*} str The input string to be reversed
+ * @returns reversed string
+ */
 function reverseString(str) {
     let reversed = '';
     for (let i = str.length - 1; i >= 0; i--) {
