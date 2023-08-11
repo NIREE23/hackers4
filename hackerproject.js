dfdd6a0d4188c3f781f6da6e8ffa51147fae424f
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-11 14:57:41 +0530

    Docs: JS docs added to check_prop feature

--- hackerproject.js
+++ hackerproject.js
@@ -1,4 +1,4 @@
-
+//Function is used to check if a property exists in a object and check if the property has certain value.
 var person={
     name:"Krithika",
     age:23,
@@ -6,6 +6,12 @@ var person={
     profession:"Trainee",
 }
 
+/**
+ * 
+ * @param {*} key
+ * @param {*} value 
+ * @returns boolean value if object has that key and if the key has certain value
+ */
 function prop_exists(key,value){
     console.log(person[key])
     if(person.hasOwnProperty(key)&&person[key]==value){
