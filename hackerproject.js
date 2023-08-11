9d98473e5f549b2e1a0342bc5eb42ce97172bc78
pr-th-m <pratham.dv@codecraft.co.in>
2023-08-11 15:11:07 +0530

    feat: added the appropriate function

--- hackerproject.js
+++ hackerproject.js
@@ -1,1 +1,8 @@
 //Author - Pratham
+//Author - Pratham
+/**
+ * This function takes a string and a letter as a parameter
+ * This function does'nt return anything, instead it prints the frequency of that letter in the passed string
+ * @param {string} str 
+ * @param {character} letter 
+ */
